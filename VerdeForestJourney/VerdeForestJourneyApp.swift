be87dd4f536134005b10dad1638286f41813627a
Oleg <yakushinoa@gmail.com>
2024-03-24 22:10:19 +0100

    Initial Commit

---
+++ VerdeForestJourney/VerdeForestJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerdeForestJourneyApp.swift
+//  VerdeForestJourney
+//
+//  Created by Oleg Yakushin on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct VerdeForestJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
